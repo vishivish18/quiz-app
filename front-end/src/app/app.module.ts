dded885955dd32fa708d0b8578972b2909de222b
Lagnesh Thakur <lagneshthakur@gmail.com>
2018-03-11 02:48:50 +0530

    Revert "Added Registration form - view "

--- front-end/src/app/app.module.ts
+++ front-end/src/app/app.module.ts
@@ -1,73 +1,66 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 import {RouterModule} from '@angular/router';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 
-import { AppComponent } from './app.component';
-import { NavBarComponent } from './nav-bar/nav-bar.component';
-import { RegisterComponent } from './register/register.component';
-import { ManageQuizComponent } from './manage-quiz/manage-quiz.component';
-import { NotFoundComponent } from './not-found/not-found.component';
+
+import {AppComponent} from './app.component';
+import {NavBarComponent} from './nav-bar/nav-bar.component';
+import {HomeComponent} from './home/home.component';
+import {ManageQuizComponent} from './manage-quiz/manage-quiz.component';
+import {NotFoundComponent} from './not-found/not-found.component';
 import {SampleComponent} from './sample/sample.component';
 
-import {FormsModule , ReactiveFormsModule} from '@angular/forms';
-
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-
 // Material Imports
-import {MatSelectModule, MatFormField, MatButtonModule, MatCheckboxModule,
-  MatCardModule, MatExpansionModule, MatIconModule, MatFormFieldModule,
-  MatInputModule, MatDatepickerModule, MatNativeDateModule} from '@angular/material';
+import {MatButtonModule, MatCheckboxModule, MatCardModule, MatExpansionModule, MatIconModule, MatFormFieldModule, MatInputModule, MatDatepickerModule, MatNativeDateModule} from '@angular/material';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     NavBarComponent,
-    RegisterComponent,
+    HomeComponent,
     ManageQuizComponent,
     NotFoundComponent,
     SampleComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule,
-    ReactiveFormsModule,
-    BrowserAnimationsModule,
-    MatFormFieldModule,
-    MatInputModule,
-    MatCardModule,
-    MatSelectModule,
     BrowserAnimationsModule,
     FormsModule,
     ReactiveFormsModule,
-    MatButtonModule,
-    MatCheckboxModule,
-    MatExpansionModule,
-    MatIconModule,
-    MatDatepickerModule,
-    MatNativeDateModule,
     RouterModule.forRoot([
       {
         path: '',
-        component: RegisterComponent
+        component: HomeComponent
       },
     {
-      path: 'managequiz',
-      component : ManageQuizComponent
-    },
-    {
-      path: 'home' ,
-      component: RegisterComponent
+      path:'managequiz' , 
+      component : ManageQuizComponent 
+    }, 
+    { 
+      path:'home' ,
+      component:HomeComponent
+    } ,
+    { 
+      path:'sample' ,
+      component:SampleComponent
     } ,
     {
-      path: 'sample' ,
-      component: SampleComponent
-    } ,
-    {
-      path: '**',
-      component: NotFoundComponent
+      path:'**',
+      component:NotFoundComponent
     }
-    ])
+    ]),
+    MatButtonModule,
+    MatCheckboxModule,
+    MatCardModule,
+    MatExpansionModule,
+    MatIconModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatDatepickerModule,
+    MatNativeDateModule
   ],
   providers: [],
   bootstrap: [AppComponent]
