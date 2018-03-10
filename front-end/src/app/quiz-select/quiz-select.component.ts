7305cbc64f52b65b0011fb01d8aef18e05828784
Aastha Goel <aasthagoel96@gmail.com>
2018-03-11 00:09:15 +0530

    Added quiz-select component

---
+++ front-end/src/app/quiz-select/quiz-select.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+
+@Component({
+  selector: 'app-quiz-select',
+  templateUrl: './quiz-select.component.html',
+  styleUrls: ['./quiz-select.component.css']
+})
+export class QuizSelectComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
