bf5ded2284da086941e46efa836f2b83fa2b3df0
Neha Singh <neha.singh@cybergindia.local>
2018-03-11 00:16:03 +0530

    changes in frontend

--- front-end/src/app/app.module.ts
+++ front-end/src/app/app.module.ts
@@ -1,12 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import {RouterModule} from '@angular/router'
+import {RouterModule} from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { HomeComponent } from './home/home.component';
 import { ManageQuizComponent } from './manage-quiz/manage-quiz.component';
 import { NotFoundComponent } from './not-found/not-found.component';
+import {FormsModule , ReactiveFormsModule} from '@angular/forms';
+
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatFormField, MatFormFieldModule} from '@angular/material';
+import {MatInputModule} from '@angular/material/input';
+import {MatCardModule} from '@angular/material/card';
+import {MatSelectModule} from '@angular/material/select';
+import {MatButtonModule} from '@angular/material/button';
+
 
 
 @NgModule({
@@ -18,23 +27,31 @@ import { NotFoundComponent } from './not-found/not-found.component';
     NotFoundComponent
   ],
   imports: [
-    BrowserModule, 
+    BrowserModule,
+    FormsModule,
+    ReactiveFormsModule,
+    BrowserAnimationsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatCardModule,
+    MatSelectModule,
+    MatButtonModule,
     RouterModule.forRoot([
       {
-        path:'',
+        path: '',
         component : HomeComponent
       },
     {
-      path:'managequiz' , 
-      component : ManageQuizComponent 
-    }, 
-    { 
-      path:'home' ,
-      component:HomeComponent
+      path: 'managequiz',
+      component : ManageQuizComponent
+    },
+    {
+      path: 'home' ,
+      component: HomeComponent
     } ,
     {
-      path:'**',
-      component:NotFoundComponent
+      path: '**',
+      component: NotFoundComponent
     }
 
     ])
--- front-end/src/app/home/home.component.ts
+++ front-end/src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {FormGroup , FormBuilder} from '@angular/forms';
 
 @Component({
   selector: 'app-home',
@@ -7,9 +8,48 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  loginForm: FormGroup ;
+  constructor(private _formBuilder: FormBuilder) { }
+
+
+  roles = [
+          {
+            id : 1 ,
+            value : 'Presentor'},
+            {
+            id : 2 ,
+            value : 'participant'}];
 
   ngOnInit() {
+    this.loginForm = this._formBuilder.group({
+      userName : ['' , ],
+      password : ['' , ],
+      confirmPassword : ['', ],
+      emailAddress : ['', ],
+      cgiCode : ['' , ],
+      role : ['', ]
+    });
+  }
+
+  submit(loginForm) {
+    // tslint:disable-next-line:no-debugger
+    debugger;
+    const loginData = {
+      userName : this.loginForm.controls.userName.value as string ,
+      emailAddress : this.loginForm.controls.emailAddress.value as string ,
+      cgiCode : this.loginForm.controls.cgiCode.value as string,
+      role : this.loginForm.controls.role.value as string,
+      password : this.loginForm.controls.password.value as string ,
+      confirmPassword : this.loginForm.controls.confirmPassword.value as string
+    };
+    console.log('username :' + loginData.userName  +
+                '   emailAddress: ' + loginData.emailAddress +
+                '   cgiCode: '  + loginData.cgiCode +
+                '   role: ' + loginData.role +
+                '   password :' + loginData.password  +
+                '   confirm password :' + loginData.confirmPassword
+                 );
+
   }
 
 }
